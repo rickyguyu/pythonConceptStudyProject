3cadd9c587152539c78ea2c63e1c2d740912104d
Ricky <rickyguyu@gmail.com>
2022-04-08 03:40:11 -0400

    a

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'USER': 'root',
         'NAME': 'mysiteTestDB',
         'PASSWORD': 'ricky',
-        'HOST': '192.168.20.4',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }

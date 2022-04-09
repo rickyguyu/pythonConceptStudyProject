6ebc025af3fb81f8a419c34a0c8306ed5178e396
Ricky <rickyguyu@gmail.com>
2022-04-09 07:37:13 -0400

    a

--- unitedsite/polls/urls.py
+++ unitedsite/polls/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('login/', views.login_view),
     path('toregister/', views.toregister_view),
     path('register/', views.register_view),
+    path('main/', views.main_view),
+    path('newBusiness/', views.new_business),
+    path('savebusiness/', views.savebusiness),
 ]
--- unitedsite/polls/views.py
+++ unitedsite/polls/views.py
@@ -2,33 +2,39 @@ import random
 
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Businessinfo
 from .models import Userinfo
-#  from .models import Businessinfo
+
+
 # Create your views here.
 
 def toLogin_view(request):
-    return render(request,"login.html")
+    return render(request, "login.html")
+
 
 def login_view(request):
-    u = request.POST.get("user","")
-    p = request.POST.get("pwd","")
+    u = request.POST.get("user", "")
+    p = request.POST.get("pwd", "")
     if u and p:
-        c = Userinfo.objects.filter(user_name=u,user_pwd=p).count()
-        if c==1:
+        c = Userinfo.objects.filter(user_name=u, user_pwd=p).count()
+        if c == 1:
             return HttpResponse("登陆成功")
         else:
             return HttpResponse("用户名或密码错误")
     else:
         return HttpResponse("请输入用户名和密码")
 
+
 def toregister_view(request):
-    return render(request,"registry.html")
+    return render(request, "registry.html")
+
 
 def register_view(request):
     u = request.POST.get("user", "")
     p = request.POST.get("pwd", "")
     if u and p:
-        user = Userinfo(id=str(random.randrange(0000,9999)),user_name=u,user_pwd=p)
+        user = Userinfo(id=str(random.randrange(0000, 9999)), user_name=u, user_pwd=p)
         user.save()
         return HttpResponse("注册成功")
     else:
@@ -36,7 +42,12 @@ def register_view(request):
 
 
 def main_view(request):
-    return render(request,"main.html")
+    businessinfos = Businessinfo.objects.order_by('idbusinessinfo')
+    return render(request, "main.html", {"businessLists": businessinfos})
 
+def new_business(request):
+    return render(request, "newbusiness.html")
 
-
+def savebusiness(request):
+    businessinfos = Businessinfo.objects.order_by('idbusinessinfo')
+    return render(request, "main.html", {"businessLists": businessinfos})
